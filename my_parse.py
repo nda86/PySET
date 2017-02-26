ac8ee7cc4abf0e17df7f2ed6f459a783589fe19a
nda <nda86@mail.ru>
2017-02-26 20:27:01 +0800

    add goods2

--- my_parse.py
+++ my_parse.py
@@ -5,6 +5,15 @@ def parse_cards(path):
 	with open(path, "r") as oldfile:
 		lines = oldfile.readlines()
 		for line in lines:
-			out = out + line.replace("percentage-discount", "ololo").replace("Client", "client")
+			out = out + line.replace("discount-percent","percentage-discount").replace("Client", "client")
+	out64 = base64.b64encode(out)
+	return out64
+
+def parse_goods(path):
+	out = ""
+	with open(path, "r") as oldfile:
+		lines = oldfile.readlines()
+		for line in lines:
+			out = out + line.replace('type="MAX_DISCOUNT_PERCENT" value="100.00"', 'type="MAX_DISCOUNT_PERCENT" value="0.0"').replace('<delete-from-cash>true</delete-from-cash>', '<delete-from-cash>false</delete-from-cash>')
 	out64 = base64.b64encode(out)
 	return out64
